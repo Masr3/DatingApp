7f6a897d0a3e4f3c973b175d8876a233db43e88a
masr3 <manuelsantanar03@gmail.com>
2023-12-27 02:09:42 -0400

    Added more components

---
+++ client/src/app/lists/lists.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-lists',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './lists.component.html',
+  styleUrl: './lists.component.css'
+})
+export class ListsComponent {
+
+}
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from '../services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../models/user';
+import { Router, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']  // Change styleUrl to styleUrls
 })
@@ -18,21 +19,20 @@ export class NavComponent implements OnInit {
 
   dropdownVisible: boolean = false;
 
-  constructor(public accountService:AccountService){}
+  constructor(public accountService:AccountService, private router:Router){}
   ngOnInit(): void {
   }
 
   login(){
     this.accountService.login(this.model).subscribe({
-      next:response => {
-        console.log(response)
-      },
+      next: () => this.router.navigateByUrl('/members'),
       error: error=> console.log(error)
       
     })
   }
   logout(){
-    this.accountService.logout();     
+    this.accountService.logout(); 
+    this.router.navigateByUrl('/');
   }
 
   toggleDropdown(){
